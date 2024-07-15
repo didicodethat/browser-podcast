08acdba6e69b459922d4d8073d21db6ef3b7d5e7
Mauricio <mauricio.fidalgo1@gmail.com>
2024-07-15 06:38:42 -0300

    starting the repository

---
+++ src/App.tsx
@@ -0,0 +1,107 @@
+import {useEffect, useState} from 'react'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import './App.css'
+
+const podcastURLs = [
+  "https://feeds.buzzsprout.com/1497970.rss",
+  "https://feeds.simplecast.com/5nKJV82u"
+]
+//const podcastURL = 
+// const podcastURL = "https://www.relay.fm/cortex/feed"
+const podcastURL = podcastURLs[0]
+type ImageURL = string;
+type AudioURL = string;
+
+  function App() {
+    return (
+      <MantineProvider>
+        <PodcastInfo />
+      </MantineProvider>
+    )
+  }
+
+  interface Podcast {
+    name: string,
+    thumbnailUrl: ImageURL
+    episodes: Episode[]
+  }
+
+  interface Episode {
+    name: string,
+    description: string,
+    audioUrl: AudioURL,
+    thumbnailUrl?: ImageURL
+  }
+
+  function parsePodcast(doc : XMLDocument) : Pocast {
+    console.log(doc)
+    const el = doc.querySelector("rss>channel>title") as Element
+    const name = el.innerHTML
+    const thumbnailUrl = doc.querySelector("rss>channel>image>url")?.innerHTML || ""
+    const episodes = Array.from(doc.querySelectorAll("rss>channel>item").values().map((el: Element) : Episode => {
+      return {
+        name: el.querySelector("title")?.innerHTML,
+        audioUrl: el.querySelector("enclosure")?.getAttribute("url") || "",
+        description: el.querySelector("summary")?.innerHTML,
+        thumbnailUrl: el.querySelector("image")?.getAttribute("href")
+      }
+    })).slice(0, 10)
+    return {name, thumbnailUrl, episodes}
+  }
+
+
+  function PodcastInfo() {
+    const [podcast, setPodcast] = useState<Podcast>(null)
+    const [err, setErr] = useState<Error>(null)
+    async function getPodcast() : Promise<Podcast> {
+      return fetch(podcastURL)
+        .then(response => response.text())
+        .then(content => {
+          const parser = new DOMParser()
+          return parser.parseFromString(content, "application/xml")
+        })
+        .then(parsePodcast)
+    }
+
+    useEffect(() => {
+      (async () => {
+        try {
+          setPodcast(await getPodcast())
+        } catch (e) {
+          setErr(e)
+        }
+      })()
+    }, [])
+
+    if (err) {
+      console.error(err)
+      return <>Couldn't Fetch Data</>
+    }
+    
+    if (!podcast) {
+      return <>Loading...</>
+    }
+    console.log(podcast)
+    return (<>
+      <img className="podcastThumbnail" src={podcast.thumbnailUrl} /> 
+      <h1>{podcast.name}</h1>
+      <div>{podcast.episodes.map((episode) => <PodcastEpisode episode={episode} />)}</div>
+    </>)
+  }
+
+  function PodcastEpisode({episode} : {episode: Episode}) {
+    return (
+      <article>
+        {episode.thumbnailURL && (
+          <div><img className="podcastThumbnail" src={episode.thumbnailUrl}/></div>
+      )}
+      <h3>{episode.name}</h3>
+      <div>{episode.description}</div>
+      <div><audio controls src={episode.audioUrl}/></div>
+    </article>
+  )
+
+}
+
+export default App
